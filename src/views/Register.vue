f3ac43390d7b3dfa199d8701d2b78980f668bb10
YZY05-del <yeziyu_05@qq.com>
2025-09-21 13:55:48 +0800

    Update Register.vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,11 +3,6 @@
     class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
   >
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img
-        class="mx-auto h-24 w-auto"
-        src="/powersync_logo.png"
-        alt="Your Company"
-      />
       <h2
         class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
       >
